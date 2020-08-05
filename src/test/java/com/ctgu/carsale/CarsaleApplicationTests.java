0f7c2164992373993a679913879e2f08feb5b8a9
2017166132 <2523833989@qq.com>
2020-08-05 19:45:15 +0800

    first commit

---
+++ src/test/java/com/ctgu/carsale/CarsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctgu.carsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
